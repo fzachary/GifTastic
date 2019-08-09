e1df2fe4eaaaa8b62749a3a5ff8d1a3adb0794d9
Frank Zachary <fzachary@gmail.com>
2019-08-08 23:58:42 -0400

    fixed animations on the extra gifs and added description to ReadMe

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -109,7 +109,21 @@ function displayGifInfo() {
                     gifDiv.append(displayGif);
 
                      $("#gifs-div").prepend(gifDiv);
-                     }
+                }
+
+                    $("img").on("click", function() {
+    
+                    var state = $(this).attr("data-state");
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                    })
+
             })})
 
             
